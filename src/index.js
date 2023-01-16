0557e5b782582a585e005170cc220eadd8cbedd3
Dzmitry <Dzmitrydavidovich89@gmail.com>
2023-01-16 19:03:23 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = {}; 
+  bracketsConfig.forEach((el)=> {
+        let key = el[1];
+        let value = el[0]; 
+        brackets[key] = value;
+  })
+    
+  let stack = [];
+  for (let i = 0; i < str.length; i++){
+    let current = str[i];
+    let top = stack[stack.length - 1];
+    if ((brackets[current]!=undefined)&&(brackets[current] === top)){
+      stack.pop();
+    } else {
+      stack.push(current);        
+    }
+  }  
+  
+  return stack.length === 0; 
+  
+  
 }
